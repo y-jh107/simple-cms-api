ebfc6b01e712df768ac742aed419463bfcde9d2d
양진혁 <jin99545@gmail.com>
2026-03-05 23:18:02 +0900

    feat: JwtUtil 구현

---
+++ src/main/java/com/malgn/security/JwtUtil.java
@@ -0,0 +1,79 @@
+package com.malgn.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.crypto.SecretKey;
+import java.io.Serializable;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public class JwtUtil implements Serializable {
+    private static final long serialVersionUID = -2550185165626007488L;
+    public static final long JWT_VALIDITY = 5 * 60 * 60;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    private <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        byte[] keyBytes = Base64.getDecoder().decode(secret);
+        SecretKey signingKey = Keys.hmacShaKeyFor(keyBytes);
+
+        return Jwts.parser().verifyWith(signingKey).build().parseSignedClaims(token).getPayload();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public Long getUserIdFromToken(String token) {
+        String jwt;
+        if (token.startsWith("Bearer ")) {
+            jwt = token.substring(7);
+        } else {
+            jwt = token;
+        }
+
+        return getClaimFromToken(jwt, claims -> claims.get("userId", Long.class));
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public boolean isTokenExpired(String token) {
+        Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    public String generateToken(UserDetails userDetails, Long userId) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("userId", userId);
+
+        SecretKey signingKey = Keys.hmacShaKeyFor(Base64.getDecoder().decode(secret));
+
+        return Jwts.builder()
+                .claims(claims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + JWT_VALIDITY * 1000))
+                .signWith(signingKey)
+                .compact();
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+}
