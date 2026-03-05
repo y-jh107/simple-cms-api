c28105f6605d741249fcbf664835f260430501ea
양진혁 <jin99545@gmail.com>
2026-03-06 00:01:28 +0900

    feat: PasswordEncoder, AuthenticationManager 추가 및 SecurityFilterChain 커스텀

--- src/main/java/com/malgn/configure/security/SecurityConfiguration.java
+++ src/main/java/com/malgn/configure/security/SecurityConfiguration.java
@@ -1,28 +1,80 @@
 package com.malgn.configure.security;
 
+import com.malgn.security.JwtAuthenticationEntryPoint;
+import com.malgn.security.JwtRequestFilter;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Autowired
+    public SecurityConfiguration(
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtRequestFilter jwtRequestFilter
+    ) {
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
 
     @Bean
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean(
+            AuthenticationConfiguration configuration
+    ) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
 
         http.authorizeHttpRequests(
             request ->
-                request.anyRequest().authenticated());
+                request.requestMatchers(
+                        "/api/v1/auth/login",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**")
+                .permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/v1/user")
+                .permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/v1/user/{userId}")
+                .permitAll()
+                .requestMatchers(HttpMethod.DELETE, "/api/v1/user/{userId}")
+                .hasRole("ADMIN")
+                .anyRequest().authenticated())
+            .exceptionHandling(exceptionHandling ->
+                    exceptionHandling.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+            .sessionManagement(sessionManagement ->
+                    sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.csrf(AbstractHttpConfigurer::disable);
         http.cors(AbstractHttpConfigurer::disable);
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
