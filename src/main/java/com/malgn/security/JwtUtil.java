baa138a050e49bc2cf58bf83285164ddc145bb47
양진혁 <jin99545@gmail.com>
2026-03-05 23:21:21 +0900

    feat: JwtUtil 빈 등록

--- src/main/java/com/malgn/security/JwtUtil.java
+++ src/main/java/com/malgn/security/JwtUtil.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.io.Serializable;
@@ -14,6 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+@Component
 public class JwtUtil implements Serializable {
     private static final long serialVersionUID = -2550185165626007488L;
     public static final long JWT_VALIDITY = 5 * 60 * 60;
